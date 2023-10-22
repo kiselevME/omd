e0034bcaaeab8de5b1a9dcac610dc1a953bb9ba7
Kiselev Maxim <maximkiselev8376@gmail.com>
2023-10-22 22:20:03 +0300

    Added TF-IDF vectorizer

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+from math import log
 
 
 class CountVectorizer:
@@ -9,10 +10,10 @@ class CountVectorizer:
         tokenizing. Defaults to True.
     """
 
-    def __init__(self, lowercase: bool = True):
+    def __init__(self, lowercase: bool = True) -> None:
         self.lowercase = lowercase
 
-    def _preprocessing(self, raw_documents):
+    def _preprocessing(self, raw_documents: Iterable) -> list:
         """Preprocessing of the input corpus of texts."""
         for i, doc in enumerate(raw_documents):
             if not isinstance(doc, str):
@@ -21,7 +22,7 @@ class CountVectorizer:
                 raw_documents[i] = doc.lower()
         return raw_documents
 
-    def _create_vocabulary(self, raw_documents):
+    def _create_vocabulary(self, raw_documents: Iterable) -> None:
         """Сreates a vocabulary from corpus words."""
         # использую словарь, т.к. его ключи упорядочены
         voc = {}
@@ -30,7 +31,7 @@ class CountVectorizer:
                 voc[word] = voc.get(word, "")
         self._vocabulary = list(voc.keys())
 
-    def _create_token_matrix(self, raw_documents):
+    def _create_token_matrix(self, raw_documents: Iterable) -> None:
         """Creates and returns a token matrix. Before using this function,
         you need to call _create_vocabulary."""
         self._token_matrix = []
@@ -42,7 +43,7 @@ class CountVectorizer:
             # в следующей строке пользуюсь упорядоченностью словаря
             self._token_matrix.append(list(count_words.values()))
 
-    def _check_vocabulary(self):
+    def _check_vocabulary(self) -> None:
         """Check if vocabulary is empty or missing (not fitted)."""
         if not hasattr(self, "_vocabulary"):
             raise AttributeError("Vocabulary not fitted or provided")
@@ -80,3 +81,47 @@ class CountVectorizer:
         """
         self._check_vocabulary()
         return self._vocabulary
+
+
+class TfidfTransformer:
+    @staticmethod
+    def tf_transform(count: Iterable) -> list:
+        tf = [[item / sum(doc) for item in doc] for doc in count]
+        return tf
+
+    @staticmethod
+    def idf_transform(count: Iterable) -> list:
+        docs_total = len(count)
+        voc_size = len(count[0])
+        # Документы со словом
+        docs_with_word = [sum([1 if count[i][j] > 0 else 0
+                               for i in range(docs_total)])
+                          for j in range(voc_size)]
+        # Применяем формулу для IDF
+        idf = [log((docs_total + 1) / (cnt + 1)) + 1 for cnt in docs_with_word]
+        return idf
+
+    def fit_transform(self, count_corpus: Iterable) -> list:
+        tf_corp = self.tf_transform(count_corpus)
+        idf_corp = self.idf_transform(count_corpus)
+        tf_idf = [[round(tf*idf, 3) for tf, idf in zip(tf_line, idf_corp)]
+                  for tf_line in tf_corp]
+        return tf_idf
+
+
+class TFIDF_Vectorizer(CountVectorizer):
+    def __init__(self) -> None:
+        super().__init__()
+        self.transformer = TfidfTransformer()
+
+    def fit_transform(self, corpus: Iterable) -> list:
+        count_matrix = super().fit_transform(corpus)
+        return self.transformer.fit_transform(count_matrix)
+
+
+if __name__ == '__main__':
+    corpus1 = ['Crock Pot Pasta Never boil pasta again',
+               'Pasta Pomodoro Fresh ingredients Parmesan to taste']
+
+    tf_idf = TFIDF_Vectorizer()
+    print(tf_idf.fit_transform(corpus1))
